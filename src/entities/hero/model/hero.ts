4923cb115c9772ec495c8bc6421e2228ed69e84f
Alexander Kataev <dkeyman@mail.ru>
2024-08-19 13:02:30 +0300

    добавил класс Hero

---
+++ src/entities/hero/model/hero.ts
@@ -0,0 +1,107 @@
+interface HeroCreateDto {
+  id: number
+  x: number
+  y: number
+  radius: number
+  color: string
+  direction: number
+  speed: number
+  colorAttack: string
+  speedAttack: number
+  directionAttack: number
+  spells: Spell[]
+}
+
+export interface Spell {
+  id: number
+  x: number
+  y: number
+  radius: number
+  color: string
+  direction: number
+  speed: number
+}
+
+export class Hero {
+  public id: number
+  public x: number
+  public y: number
+  public radius: number
+  public color: string
+  public direction: number
+  public speed: number
+  public colorAttack: string
+  public speedAttack: number
+  public directionAttack: number
+  public spells: Spell[] = []
+
+  constructor({
+    id,
+    x,
+    y,
+    radius,
+    color,
+    direction,
+    speed,
+    colorAttack,
+    speedAttack,
+    directionAttack,
+    spells = [],
+  }: HeroCreateDto) {
+    this.id = id
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.direction = direction
+    this.speed = speed
+    this.colorAttack = colorAttack
+    this.speedAttack = speedAttack
+    this.directionAttack = directionAttack
+    this.spells = spells
+  }
+
+  setSpeed(speed: number) {
+    this.speed = speed
+  }
+
+  update({
+    id,
+    x,
+    y,
+    radius,
+    color,
+    direction,
+    speed,
+    colorAttack,
+    speedAttack,
+    directionAttack,
+    spells,
+  }: Partial<HeroCreateDto>) {
+    Object.assign(this, {
+      id,
+      x,
+      y,
+      radius,
+      color,
+      direction,
+      speed,
+      colorAttack,
+      speedAttack,
+      directionAttack,
+      spells,
+    })
+  }
+
+  attack() {
+    this.spells.push({
+      id: Math.random(),
+      color: this.colorAttack,
+      direction: this.directionAttack,
+      radius: 10,
+      speed: this.speedAttack,
+      x: this.x,
+      y: this.y,
+    })
+  }
+}
